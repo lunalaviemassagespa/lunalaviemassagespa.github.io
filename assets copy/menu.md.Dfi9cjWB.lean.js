fd716c0d871fd90fc6ac0c5d118860293974bae0
inosukesama <22492042+inosukesama@users.noreply.github.com>
2024-08-17 16:58:47 +0800

    init

---
+++ assets copy/menu.md.Dfi9cjWB.lean.js
@@ -0,0 +1,1 @@
+import{_ as t,c as e,o as l,a2 as d}from"./chunks/framework.CEgrwLex.js";const S=JSON.parse('{"title":"Acupuncture","description":"","frontmatter":{},"headers":[],"relativePath":"menu.md","filePath":"menu.md"}'),n={name:"menu.md"},s=d("",7),r=[s];function a(i,g,x,y,c,A){return l(),e("div",null,r)}const h=t(n,[["render",a]]);export{S as __pageData,h as default};
